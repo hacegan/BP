530c96d036655b1597a75a8f593b42ad9a77890e
hacegan <samet822@hotmail.com>
2017-11-21 18:05:58 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Kirala6.java
+++ app/src/main/java/samet/com/bp/Kirala6.java
@@ -1,7 +1,11 @@
 package samet.com.bp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +17,9 @@ import android.widget.Button;
 
 public class Kirala6 extends AppCompatActivity {
     Button btn,fotocekbtn,fotosecbtn,btndvm;
+
+    int REQUEST_CAMERA=1,SELECT_FILE=1;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,6 +50,24 @@ public class Kirala6 extends AppCompatActivity {
         });
 
 
+        fotosecbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Dosyayı Seçin"),SELECT_FILE);
+            }
+        });
+
+
+        fotocekbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CAMERA);
+            }
+        });
+
 
     }
 
@@ -51,4 +76,30 @@ public class Kirala6 extends AppCompatActivity {
         Intent intent = new Intent(Kirala6.this,Kirala5.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode== Activity.RESULT_OK){
+
+            if(requestCode==REQUEST_CAMERA){
+Bundle bundle=data.getExtras();
+                final Bitmap bmp = (Bitmap) bundle.get("data");
+                //img.setImageBitmap(bmp=;
+            }
+            else if(requestCode==SELECT_FILE){
+Uri selectedImageUri=data.getData();
+                //img.setImageBitmap(bmp=;
+            }
+
+
+        }
+
+
+    }
+
+
+
+
 }

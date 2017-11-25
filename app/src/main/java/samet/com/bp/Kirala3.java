dacf898b5d9e2c43f9bdc011b239cfb111ad9d91
hacegan <samet822@hotmail.com>
2017-11-25 23:13:51 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Kirala3.java
+++ app/src/main/java/samet/com/bp/Kirala3.java
@@ -3,60 +3,54 @@ package samet.com.bp;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
+import java.util.Locale;
+
+import static android.R.attr.button;
 
 /**
  * Created by root on 20.11.2017.
  */
 
-public class Kirala3 extends AppCompatActivity {
+public class Kirala3 extends AppCompatActivity{
     Button btn, btnevet, btnhayir;
-    private FusedLocationProviderClient mFusedLocationClient;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kirala3);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                        }
-                    }
-                });
 
 
 
@@ -80,7 +74,7 @@ public class Kirala3 extends AppCompatActivity {
 
 
 
-                Intent intent = new Intent(Kirala3.this,Kirala4.class);
+                Intent intent = new Intent(Kirala3.this,Kirala5.class);
                 startActivity(intent);
             }
         });
@@ -95,9 +89,17 @@ public class Kirala3 extends AppCompatActivity {
         });
 
 
-
     }
 
 
+
+
+
+
+
+/*   SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("MyPref",0);
+SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("locations",addresses.get(0).getAddressLine(0)+addresses.get(0).getAddressLine(1)+addresses.get(0).getAddressLine(2));
+            editor.commit();*/
 
 }
--- app/src/main/java/samet/com/bp/Kirala5.java
+++ app/src/main/java/samet/com/bp/Kirala5.java
@@ -1,11 +1,13 @@
 package samet.com.bp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by root on 20.11.2017.
@@ -13,12 +15,19 @@ import android.widget.Button;
 
 public class Kirala5 extends AppCompatActivity {
     Button btn;
-
+TextView tv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kirala5);
         btn= (Button) findViewById(R.id.geribtn);
+        tv= (TextView) findViewById(R.id.locadresid);
+
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        tv.setText(sharedPref.getString("locations",null));
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

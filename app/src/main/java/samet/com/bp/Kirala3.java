a2ac3ab1a1e448e66ba899edb05f04953acf2b94
hacegan <samet822@hotmail.com>
2017-11-25 19:44:17 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Kirala3.java
+++ app/src/main/java/samet/com/bp/Kirala3.java
@@ -16,23 +16,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.util.List;
 
 /**
  * Created by root on 20.11.2017.
  */
 
-public class Kirala3  extends AppCompatActivity implements LocationListener{
-    Button btn,btnevet,btnhayir;
-    private LocationManager locationManager;
-    private double longtitude;
-    private double latitude;
+public class Kirala3 extends AppCompatActivity {
+    Button btn, btnevet, btnhayir;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kirala3);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
 
 
 
@@ -54,18 +78,7 @@ public class Kirala3  extends AppCompatActivity implements LocationListener{
             @Override
             public void onClick(View v) {
 
-                locationManager= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-
-                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-
-                }
-
-Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
- onLocationChanged(location);//enlem ve boylam için
-                loc_func(location);//ülke şehir için
 
                 Intent intent = new Intent(Kirala3.this,Kirala4.class);
                 startActivity(intent);
@@ -84,57 +97,6 @@ Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_P
 
 
     }
-
-    public void onLocationChanged(Location location){
-longtitude=location.getLongitude();
-        latitude=location.getLatitude();
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(Kirala3.this,Kirala2.class);
-        startActivity(intent);
-    }
-
-//Şuanki şehri , ülkeyi almak için
-private void loc_func(Location location){
-
-try{
-    Geocoder geocoder=new Geocoder(this);
-    List<Address> addresses=null;
-    addresses=geocoder.getFromLocation(latitude,longtitude,1);
-    String country=addresses.get(0).getCountryName();//Ülke
-String city=addresses.get(0).getLocality();//Şehir
-    String state=addresses.get(0).getAdminArea();//İlçe
-   String postakodu= addresses.get(0).getPostalCode();//Posta kodu
-
-//settext(yazdir);
-
-}
-catch (Exception e){
-    System.out.println("Şehri ve ülkeyi almada hata : "+e.getCause());
-}
-
-
-
-}
 
 
 

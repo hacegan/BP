ccccc728b2dfa795e6fc69bd1eb04e6430671266
hacegan <samet822@hotmail.com>
2017-11-26 13:18:10 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Kirala3.java
+++ app/src/main/java/samet/com/bp/Kirala3.java
@@ -3,48 +3,54 @@ package samet.com.bp;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 import java.util.Locale;
 
-import static android.R.attr.button;
 
 /**
  * Created by root on 20.11.2017.
  */
 
-public class Kirala3 extends AppCompatActivity{
+public class Kirala3 extends AppCompatActivity {
     Button btn, btnevet, btnhayir;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    double latitude,longitude;
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
 
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +62,64 @@ public class Kirala3 extends AppCompatActivity{
 
         btnevet= (Button) findViewById(R.id.btnevet);
         btnhayir= (Button) findViewById(R.id.btnhayir);
+
+         locationManager= (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+         locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("My Location",location.toString());
+               latitude= location.getLatitude();
+                longitude=location.getLongitude();
+                String strAdd = "";
+                Geocoder geocoder = new Geocoder(Kirala3.this, Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                    if (addresses != null) {
+                        Address returnedAddress = addresses.get(0);
+                        StringBuilder strReturnedAddress = new StringBuilder("");
+
+                        for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+                            strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                        }
+                        strAdd = strReturnedAddress.toString();
+                        Log.w("My Current address", strReturnedAddress.toString());
+                    } else {
+                        Log.w("My Current address", "No Address returned!");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.w("My Current address", "Canont get Address!");
+                }
+
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
 
 
         btn= (Button) findViewById(R.id.geribtn);

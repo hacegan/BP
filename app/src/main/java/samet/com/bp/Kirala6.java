954e8acbad4aebbdddd6af861df5d941f7825297
hacegan <samet822@hotmail.com>
2017-12-03 16:59:56 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Kirala6.java
+++ app/src/main/java/samet/com/bp/Kirala6.java
@@ -2,14 +2,24 @@ package samet.com.bp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+
 
 /**
  * Created by root on 20.11.2017.
@@ -17,8 +27,10 @@ import android.widget.Button;
 
 public class Kirala6 extends AppCompatActivity {
     Button btn,fotocekbtn,fotosecbtn,btndvm;
+ImageView iv;
 
-    int REQUEST_CAMERA=1,SELECT_FILE=1;
+    Uri selectedImageUri;
+    String  selectedPath;
 
 
     @Override
@@ -29,6 +41,7 @@ public class Kirala6 extends AppCompatActivity {
         fotocekbtn= (Button) findViewById(R.id.fotocekbtn);
         fotosecbtn= (Button) findViewById(R.id.fotosecbtn);
         btndvm= (Button) findViewById(R.id.btndvm);
+        iv= (ImageView) findViewById(R.id.camerapic);
 
 
         btn= (Button) findViewById(R.id.geribtn);
@@ -53,7 +66,7 @@ public class Kirala6 extends AppCompatActivity {
         fotosecbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openGallery(10);
             }
         });
 
@@ -61,9 +74,13 @@ public class Kirala6 extends AppCompatActivity {
         fotocekbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 100);
             }
-        });
+            });
+
+
+
 
 
     }
@@ -73,10 +90,63 @@ public class Kirala6 extends AppCompatActivity {
         Intent intent = new Intent(Kirala6.this,Kirala5.class);
         startActivity(intent);
     }
+    public void openGallery(int req_code){
+
+        Intent intent = new Intent();
+
+        intent.setType("image/*");
+
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent,"Select file to upload "), req_code);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 
 
+        if (resultCode == RESULT_OK) {
+            if(data.getData() != null){
+                selectedImageUri = data.getData();
+            }else{
+                Log.d("selectedPath1 : ","Came here its null !");
+                Toast.makeText(getApplicationContext(), "failed to get Image!", 500).show();
+            }
 
+            if (requestCode == 100 && resultCode == RESULT_OK) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                selectedPath = getPath(selectedImageUri);
+                iv.setImageURI(selectedImageUri);
+                Log.d("selectedPath1 : " ,selectedPath);
 
+            }
 
+            if (requestCode == 10)
+
+            {
+
+                selectedPath = getPath(selectedImageUri);
+                iv.setImageURI(selectedImageUri);
+                Log.d("selectedPath1 : " ,selectedPath);
+
+            }
+
+        }
+
+    }
+
+    public String getPath(Uri uri) {
+
+        String[] projection = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+
+    }
 }

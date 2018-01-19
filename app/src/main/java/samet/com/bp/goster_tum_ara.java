b45e83c9136c5a067395cdf87e940295dde840f2
hacegan <samet822@hotmail.com>
2018-01-19 19:53:46 +0300

    ilkcommit

---
+++ app/src/main/java/samet/com/bp/goster_tum_ara.java
@@ -0,0 +1,13 @@
+package samet.com.bp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class goster_tum_ara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goster_tum_ara);
+    }
+}
---
+++ app/src/main/java/samet/com/bp/goster_tum_kirala.java
@@ -0,0 +1,25 @@
+package samet.com.bp;
+
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class goster_tum_kirala extends Activity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goster_tum_kirala);
+
+    }
+
+
+}

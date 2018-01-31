fafd9a98245de19d7b115cd96a2a8bcdb4e57944
hacegan <samet822@hotmail.com>
2018-02-01 01:16:25 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/tekilarailangoster.java
+++ app/src/main/java/samet/com/bp/tekilarailangoster.java
@@ -116,48 +116,48 @@ public class tekilarailangoster extends Activity{
 
                     //  ilanid.add(  temp.substring(temp.indexOf("Kirala id:"),temp.indexOf("-",temp.indexOf("Kirala id:")) )        );
 
-                    String odaara1bay=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara1bay=temp.substring(temp.indexOf("arayanbir: "),temp.indexOf("-",temp.indexOf("arayanbir: ")) );
                     System.out.println("odaara1bay ="+odaara1bay);
 
-                    String odaara2resim=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                 //   String odaara2resim=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
                     //System.out.println("odaara2resim = "+odaara2resim);
 
-                    String odaara3yas=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara3yas=temp.substring(temp.indexOf("yas : "),temp.indexOf("-",temp.indexOf("yas : ")) );
                     System.out.println("odaara3yas = "+odaara3yas);
 
-                    String odaara4meslek=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara4meslek=temp.substring(temp.indexOf("meslek: "),temp.indexOf("-",temp.indexOf("meslek: ")) );
                     System.out.println("odaara4meslek = "+odaara4meslek);
 
-                    String odaara5evet=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara5evet=temp.substring(temp.indexOf("havepet: "),temp.indexOf("-",temp.indexOf("havepet: ")) );
                     System.out.println("odaara5evet = "+odaara5evet);
 
-                    String odaara6evet=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara6evet=temp.substring(temp.indexOf("havesmoke: "),temp.indexOf("-",temp.indexOf("havesmoke: ")) );
                     System.out.println("odaara6evet = "+odaara6evet);
 
-                    String odaara8tv =temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
-                    System.out.println("odaara8tv = "+odaara8tv);
+                  //  String odaara8tv =temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                  //  System.out.println("odaara8tv = "+odaara8tv);
 
-                    String odaarahangiil=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
-                    System.out.println("odaarahangiil = "+odaarahangiil);
+             //       String odaarahangiil=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                 //   System.out.println("odaarahangiil = "+odaarahangiil);
 
-                    String odaarahangiilce=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
-                    System.out.println("odaarahangiilce = "+odaarahangiilce);
+                 //   String odaarahangiilce=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                 //   System.out.println("odaarahangiilce = "+odaarahangiilce);
 
-                    String odaara10butce=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara10butce=temp.substring(temp.indexOf("butce: "),temp.indexOf("-",temp.indexOf("butce: ")) );
                     System.out.println("odaara10butce = "+odaara10butce);
 
-                    String odaara11tarih=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara11tarih=temp.substring(temp.indexOf("hazirbulunmatarih:"),temp.indexOf("-",temp.indexOf("hazirbulunmatarih:")) );
                     System.out.println("odaara11tarih = "+odaara11tarih);
 
-                    String odaara12sure=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara12sure=temp.substring(temp.indexOf("konaklamasure: "),temp.indexOf("-",temp.indexOf("konaklamasure: ")) );
                     System.out.println("odaara12sure = "+odaara12sure);
 
-                    String odaara13numara=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara13numara=temp.substring(temp.indexOf("telefonno: "),temp.indexOf("-",temp.indexOf("telefonno: ")) );
                     System.out.println("odaara13numara = "+odaara13numara);
 
-                    String odaara14baslik=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara14baslik=temp.substring(temp.indexOf("ilanbaslik: "),temp.indexOf("-",temp.indexOf("ilanbaslik: ")) );
 
-                    String odaara14aciklama=temp.substring(temp.indexOf("ilanbaslik:"),temp.indexOf("-",temp.indexOf("ilanbaslik:")) );
+                    String odaara14aciklama=temp.substring(temp.indexOf("ilanaciklama: "),temp.indexOf("-",temp.indexOf("ilanaciklama: ")) );
 
 
 
@@ -167,7 +167,7 @@ public class tekilarailangoster extends Activity{
                     textView.setText(ara_yayin_arayan+" "+odaara1bay);
 
 
-                    imageView= (ImageView) findViewById(R.id.ara_yayin_resim);
+                /*    imageView= (ImageView) findViewById(R.id.ara_yayin_resim);
 
                     if (!odaara2resim.equalsIgnoreCase("")) {
                         //Decoding the Image and display in ImageView
@@ -179,7 +179,7 @@ public class tekilarailangoster extends Activity{
                     {
                         Toast.makeText(getApplicationContext(),"You don't have Image in SharedPreferences!", Toast.LENGTH_SHORT).show();
                     }
-
+*/
                     textView= (TextView) findViewById(R.id.ara_yayin_yas);
                     String ara_yayin_yas=textView.getText().toString();
                     textView.setText(ara_yayin_yas+" "+odaara3yas);
@@ -197,17 +197,17 @@ public class tekilarailangoster extends Activity{
                     String ara_yayin_sigaravarmi=textView.getText().toString();
                     textView.setText(ara_yayin_sigaravarmi+" "+odaara6evet);
 
-                    textView= (TextView) findViewById(R.id.ara_yayin_yasaralik);
+                /*    textView= (TextView) findViewById(R.id.ara_yayin_yasaralik);
                     String ara_yayin_yasaralik=textView.getText().toString();
-                    textView.setText(ara_yayin_yasaralik+" "+odaara8tv);
+                    textView.setText(ara_yayin_yasaralik+" "+odaara8tv);*/
 
-                    textView= (TextView) findViewById(R.id.ara_yayin_neredelive);
+                /*    textView= (TextView) findViewById(R.id.ara_yayin_neredelive);
                     String ara_yayin_neredelive=textView.getText().toString();
-                    textView.setText(ara_yayin_neredelive+" "+odaarahangiil);
+                    textView.setText(ara_yayin_neredelive+" "+odaarahangiil);*/
 
-                    textView= (TextView) findViewById(R.id.ara_yayin_butce);
+                /*    textView= (TextView) findViewById(R.id.ara_yayin_butce);
                     String ara_yayin_butce=textView.getText().toString();
-                    textView.setText(ara_yayin_butce+" "+odaarahangiilce);
+                    textView.setText(ara_yayin_butce+" "+odaarahangiilce);*/
 
                     textView= (TextView) findViewById(R.id.ara_yayin_hazirtarih);
                     String ara_yayin_hazirtarih=textView.getText().toString();

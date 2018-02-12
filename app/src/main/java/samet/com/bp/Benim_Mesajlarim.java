0e6c27c364bd6e18051d0788008b6ce348b3c83f
hacegan <samet822@hotmail.com>
2018-02-12 16:11:38 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/Benim_Mesajlarim.java
+++ app/src/main/java/samet/com/bp/Benim_Mesajlarim.java
@@ -1,6 +1,9 @@
 package samet.com.bp;
 
+import android.app.Activity;
+import android.app.Application;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -44,7 +47,7 @@ import static samet.com.bp.Message_Details.chatWithList;
  * Created by root on 11.02.2018.
  */
 
-public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClickListener{
+public class Benim_Mesajlarim extends Activity implements  View.OnClickListener,Application.ActivityLifecycleCallbacks{
 
     DatabaseReference rootRef, demoRef;
 
@@ -57,6 +60,20 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
     static int bakmaona = 0;
 
    static LinearLayout ll;
+
+   static com.firebase.client.ValueEventListener valueEventListener;
+
+   static Firebase reference;
+    @Override
+    protected void onResume() {
+        super.onResume();
+      //  ll= (LinearLayout) findViewById(R.id.benimmesajll);
+        chatWithList.clear();
+        //ll.removeAllViews();
+
+//reference.removeEventListener(valueEventListener);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,10 +87,91 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
                 ll.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
       //  new Mesaj_Users().execute();
-
+        chatWithList.clear();
+        //ll.removeAllViews();
 
         Firebase.setAndroidContext(this);
-        Firebase reference = new Firebase("https://bitirme-proje-1511471101877.firebaseio.com/messages");
+         reference = new Firebase("https://bitirme-proje-1511471101877.firebaseio.com/messages");
+
+/*        reference.addValueEventListener(valueEventListener);
+
+  valueEventListener=new com.firebase.client.ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+
+                    System.out.println("1 = " + childDataSnapshot.getKey());//Bu emailleri alıyor sorunsuz
+                    // System.out.println(childDataSnapshot.child("user").getValue());
+
+                    String kimlekimarasinda = childDataSnapshot.getKey();
+                    StringTokenizer token = new StringTokenizer(kimlekimarasinda, "_");
+
+                    int i = 0;
+                    while (token.hasMoreTokens()) {
+
+                        String temp = token.nextToken();
+
+                        if (i == 0 && !kaynak_kisi.replace(".", ",").equals(temp)) {
+                            bakmaona = -1;
+                            break;
+
+
+                        }
+
+                        if (i == 1) {
+                            hedef_kisi = temp;
+                            if(!chatWithList.contains(temp))
+                                chatWithList.add(temp);
+
+                        }
+                        i++;
+                    }
+
+                    if (bakmaona == -1) {
+                        bakmaona = 0;
+                        continue;
+                    }
+
+
+                    for (DataSnapshot childDataSnapshot2 : childDataSnapshot.getChildren()) {
+                        System.out.println("2 = " + childDataSnapshot2.getKey());
+
+                        for (DataSnapshot childDataSnapshot3 : childDataSnapshot2.getChildren()) {
+                            System.out.println("3 = " + childDataSnapshot3.getKey());
+                            System.out.println("Mesaj = " + childDataSnapshot3.getValue());
+                        }
+
+                    }
+
+
+                }
+
+
+
+                for (int i = 0; i < chatWithList.size(); i++) {
+                    TextView tv = new TextView(Benim_Mesajlarim.this);
+                    tv.setText(chatWithList.get(i));
+                    tv.setTextSize(35);
+                    tv.setPadding(80, 80, 80, 80);
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    tv.setTextAppearance(android.R.attr.textAppearanceLarge);
+                    tv.setOnClickListener(Benim_Mesajlarim.this);
+                    tv.setTag(chatWithList.get(i));
+                    ll.addView(tv);
+                }
+
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        };*/
+
         reference.addValueEventListener(new com.firebase.client.ValueEventListener() {
             @Override
             public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
@@ -99,6 +197,7 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
 
                         if (i == 1) {
                             hedef_kisi = temp;
+                            if(!chatWithList.contains(temp))
                             chatWithList.add(temp);
 
                         }
@@ -124,6 +223,21 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
 
                 }
                 System.out.println("OnDataChange Lİste = "+chatWithList);
+
+                for (int i = 0; i < chatWithList.size(); i++) {
+                    TextView tv = new TextView(Benim_Mesajlarim.this);
+                    tv.setText(chatWithList.get(i));
+                    tv.setTextSize(35);
+                    tv.setPadding(80, 80, 80, 80);
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    tv.setTextAppearance(android.R.attr.textAppearanceLarge);
+                    tv.setOnClickListener(Benim_Mesajlarim.this);
+                    tv.setTag(chatWithList.get(i));
+                    ll.addView(tv);
+                }
+reference.removeEventListener(this);
+
+                //Firebase.goOffline();
             }
 
             @Override
@@ -134,19 +248,8 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
 
 
 
-        System.out.println("Lİste = "+chatWithList);
+        //System.out.println("Lİste = "+chatWithList);
 
-        for (int i = 0; i < chatWithList.size(); i++) {
-            TextView tv = new TextView(Benim_Mesajlarim.this);
-            tv.setText(chatWithList.get(i));
-            tv.setTextSize(35);
-            tv.setPadding(80, 80, 80, 80);
-            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            tv.setTextAppearance(android.R.attr.textAppearanceLarge);
-            tv.setOnClickListener(Benim_Mesajlarim.this);
-            tv.setTag(chatWithList.get(i));
-            ll.addView(tv);
-        }
 
     }
 
@@ -171,6 +274,48 @@ public class Benim_Mesajlarim extends AppCompatActivity implements  View.OnClick
     @Override
     public void onClick(View v) {
 
+        TextView textView=(TextView) v;
+        String hedef_kisi_sec= (String) textView.getTag();
+        UserDetails_Firebase.chatWith=hedef_kisi_sec;
+       // Firebase.goOffline();
+        getApplicationContext().startActivity(new Intent(getApplicationContext(),Chat_Firebase.class));
+
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+      //  Firebase.goOffline();
+        chatWithList.clear();
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+       // Firebase.goOffline();
     }
 /*
     public class Mesaj_Users extends AsyncTask {

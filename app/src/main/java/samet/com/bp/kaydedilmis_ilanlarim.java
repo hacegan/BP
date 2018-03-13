38560fd8c098f90801f93b99881e72b127f3eb63
hacegan <samet822@hotmail.com>
2018-03-13 13:32:45 +0300

    ilkcommit

--- app/src/main/java/samet/com/bp/kaydedilmis_ilanlarim.java
+++ app/src/main/java/samet/com/bp/kaydedilmis_ilanlarim.java
@@ -1,17 +1,32 @@
 package samet.com.bp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
@@ -20,20 +35,105 @@ import java.util.StringTokenizer;
 
 public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickListener{
 
+    LinearLayout ll;
+    static String benim_ilanlarim_url="http://vodkamorello.atspace.co.uk/kaydedilmis_ilanlarim.php";
+    static String sonuc;
+    static SharedPreferences sharedPref;
+    static SharedPreferences.Editor editor;
+    static int resultcount=0;
+    TextView resulttv;
+    ArrayList<String> ilanbaslik = new ArrayList<String>();
+    ArrayList<String> ilanaciklama = new ArrayList<String>();
+    ArrayList<String> kiralailanid=new ArrayList<String>();
+    ArrayList<String> arailanid=new ArrayList<String>();
+    static StorageReference storageReference= FirebaseStorage.getInstance().getReference();
+
+    static  String kul_mail;
+
+    static int kiralamaximgid=0;
+    static int aramaximgid=0;
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kaydedilmis_ilanlarim);
+
+        sharedPref = getApplicationContext().getSharedPreferences("MyPref",0);
+        editor = sharedPref.edit();
+
+        benim_ilanlarim_url+="?user_id="+sharedPref.getString("user_id",null);
+
+        ll= (LinearLayout) findViewById(R.id.kayitilantv);
+        ll.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        kul_mail =sharedPref.getString("email",null);
+
+
+        new MyAd().execute();
+
     }
 
     @Override
     public void onClick(View v) {
 
+        String text_tag= (String) v.getTag();
+        Intent intent;
+
+        System.out.println("Gelen Tag DEgeri = "+text_tag);
+
+        if(text_tag.contains("Kirala")){
+
+            intent=new Intent(kaydedilmis_ilanlarim.this,Benim_Tekil_Kira_Goster.class);
+            StringTokenizer token = new StringTokenizer(text_tag,":");
+
+            int i=0;
+            while (token.hasMoreTokens()) {
+
+                String temp = token.nextToken();
+
+                if(i==1){
+                    intent.putExtra("ilan_id",temp);
+                    System.out.println("GÖnderilen id ="+temp);
+                }
+                i++;
+            }
+
+
+            // Intent  intent = new Intent(Benim_ilanlarim.this,Benim_Tekil_Kira_Goster.class);
+            startActivity(intent);
+        }
+
+        else{
+
+            intent=new Intent(kaydedilmis_ilanlarim.this,Benim_Tekil_Ara_Goster.class);
+            StringTokenizer token = new StringTokenizer(text_tag,":");
+
+            int i=0;
+            while (token.hasMoreTokens()) {
+
+                String temp = token.nextToken();
+                if(i==1){
+                    intent.putExtra("ilan_id",temp);
+                    System.out.println("GÖnderilen id ="+temp);
+                }
+
+                i++;
+            }
+
+
+            //Intent  intent = new Intent(Benim_ilanlarim.this,Benim_Tekil_Ara_Goster.class);
+            startActivity(intent);
+        }
+
+
+
+
     }
 
 
-   /* public class MyAd extends AsyncTask {
+    public class MyAd extends AsyncTask {
 
 
         @Override
@@ -45,14 +145,58 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
         protected void onPostExecute(Object o) {
 
 
+            StorageReference ref = null;
+
             for(int i=0;i<resultcount;i++){
-                TextView tv=new TextView(Benim_ilanlarim.this);
+                final TextView tv=new TextView(kaydedilmis_ilanlarim.this);
 
 
+               if(i<kiralailanid.size()){
 
-                Drawable drawable=getApplicationContext().getResources().getDrawable(R.drawable.empty_house);
-                tv.setCompoundDrawablesWithIntrinsicBounds(drawable,null,null,null);
-                // tv.setCompoundDrawables(R.drawable.empty_house,null,null,null);
+                    final ImageView tempimg=new ImageView(kaydedilmis_ilanlarim.this);
+
+
+                    storageReference.child("images/herkirala/"+(i+1)).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            System.out.println("Basariilli");
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                            tempimg.setImageBitmap(bmp);
+                            Drawable drawable=tempimg.getDrawable();
+                            drawable.setBounds(0,0,460,460);
+                            tv.setCompoundDrawables(drawable,null,null,null);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            System.out.println("Basarisiz");
+                        }
+                    });
+
+
+                }
+                else{
+                    final ImageView tempimg=new ImageView(kaydedilmis_ilanlarim.this);
+
+
+                    storageReference.child("images/herara/"+   (i-kiralailanid.size()+1)     ).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            System.out.println("Basariilli");
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                            tempimg.setImageBitmap(bmp);
+                            Drawable drawable=tempimg.getDrawable();
+                            drawable.setBounds(0,0,460,460);
+                            tv.setCompoundDrawables(drawable,null,null,null);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            System.out.println("Basarisiz");
+                        }
+                    });
+                }
+
 
                 tv.setText(ilanbaslik.get(i)+"\n"+ilanaciklama.get(i));
                 //tv.setText(ilanbaslik.get(i));
@@ -66,14 +210,14 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
                     tv.setTag("Kirala:"+kiralailanid.get(i));
 
                 else{
-                    tv.setTag("Ara:"+arailanid.get(i));
+                    tv.setTag("Ara:"+arailanid.get(i-kiralailanid.size()));
                 }
 
 
 
                 // tv.setBackgroundResource(R.drawable.border_textview);
 
-                tv.setOnClickListener(Benim_ilanlarim.this);
+                tv.setOnClickListener(kaydedilmis_ilanlarim.this);
 
                 //setOnClick(tv,ilanid.get(i));
 
@@ -81,7 +225,6 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
                 ll.addView(tv);
 
             }
-//
 
 
         }
@@ -90,7 +233,7 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
         protected Object doInBackground(Object[] params) {
 
             try{
-                URL url=new URL("http://vodkamorello.atspace.co.uk/kaydedilmis_ilanlarim.php");
+                URL url=new URL(benim_ilanlarim_url);
                 HttpURLConnection con= (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
                 con.connect();
@@ -107,7 +250,7 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
 
 
                     if(!temp.contains("<br>")) {
-                        if (Integer.valueOf(temp) instanceof Integer) {
+                        if (!temp.contains("results")   && Integer.valueOf(temp) instanceof Integer) {
 
 
                             resultcount += Integer.valueOf(temp);
@@ -135,7 +278,7 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
                 }
 
                 System.out.println("Toplam Result "+resultcount);
-                resulttv= (TextView) findViewById(R.id.benimilanresultsid);
+                resulttv= (TextView) findViewById(R.id.kayitilanresultsid);
                 resulttv.setText("Toplam Sonuc : "+resultcount);
 
 
@@ -153,7 +296,7 @@ public class kaydedilmis_ilanlarim extends Activity implements  View.OnClickList
 
 
     }
-*/
+
 
 
 }
